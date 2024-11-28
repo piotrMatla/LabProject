83aff4e8e0ee05ec86d420698524786c89118c68
Piotr Matla <piotrmatla@yahoo.pl>
2024-11-28 13:45:07 +0100

    Initial commit for ASP.NET MVC project with Docker

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LabProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
