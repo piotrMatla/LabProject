03052da09302000f731240c3593f8b0f2ef68a03
Piotr Matla <piotrmatla@yahoo.pl>
2024-11-28 16:11:19 +0100

    create User model and add simple server validation

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LabProject.Models;
+
+namespace LabProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Policy;
+
+
+namespace LabProject.Models
+{
+    public class User
+    {
+        public int Id {  get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        [MinLength(2, ErrorMessage = "Name must have at least 2 characters")]
+        public string FirstName { get; set; }
+
+
+        [Required(ErrorMessage = "Last name is required")]
+        [MinLength(2, ErrorMessage = "Last name must have at least 2 characters")]
+        public string LastName {  get; set; }
+
+
+        [Required(ErrorMessage = "Email adress is required")]
+        [EmailAddress(ErrorMessage = "Incorrect email address format")]
+        public string Email {  get; set; }
+
+
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$", ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, and one digit.")]
+        [MinLength(8, ErrorMessage = "Your password must contain 8 characters")]
+        public string Password {  get; set; }
+
+
+        [Required(ErrorMessage = "Password confirmation is required")]
+        [Compare("Password", ErrorMessage = "Passwords provided must be the same")]
+        public string PasswordConfirmation {  get; set; }
+
+
+        [Phone(ErrorMessage = "Niepoprawny format numeru telefonu")]
+        public string MobileNumber {  get; set; }
+
+
+        public DateTime? DateOfBirth {  get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool AgreeToTerms {  get; set; }
+        public bool PrivacyPolicyConsent {  get; set; }
+    }
+}
