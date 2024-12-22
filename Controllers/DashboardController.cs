d37c236d81f621d645d1c4689219ed78baf48bfb
klaudia_marszalek <klaudia.marszalek.04@gmail.com>
2024-12-22 22:11:10 +0100

    sum  expenses

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Drawing.Drawing2D;
 using System.Threading.Tasks;
+using System.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
 
@@ -14,21 +16,41 @@ namespace LabProject.Controllers
     public class DashboardController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly LabProjectDbContext _context;
 
-        public DashboardController(UserManager<ApplicationUser> userManager)
+        public DashboardController(UserManager<ApplicationUser> userManager, LabProjectDbContext context)
         {
             _userManager = userManager;
+            _context = context ?? throw new ArgumentNullException(nameof(context)); // Dodatkowa kontrola null
         }
         public async Task<IActionResult> Index()
         {
             var currentUser = await _userManager.GetUserAsync(User);
+            if (_context == null)
+            {
+                // Obsługa błędu, jeśli _context jest null
+                return BadRequest("Database context is not available.");
+            }
 
-            
+            // Zsumowanie wszystkich wydatków użytkownika
+            var totalExpenses = await _context.Transaction
+                .Where(t => t.UserId == currentUser.Id)  
+                .Join(_context.Categories,
+                      t => t.CategoryId,
+                      c => c.Id,
+                      (t, c) => new { t, c })  
+                .Where(x => x.c.Type == "Expense")  
+                .SumAsync(x => x.t.Amount);  
+
+
             var FirstName = currentUser?.FirstName;
             ViewBag.Message = $"Hello, <strong class='text-primary'>{FirstName}</strong>";
             ViewBag.Layout = "_Layout_Dashboard";
+            ViewBag.Expenses = totalExpenses;
             return View();
         }
+
+        
 
         // GET: DashboardController/Details/5
         public ActionResult Details(int id)
